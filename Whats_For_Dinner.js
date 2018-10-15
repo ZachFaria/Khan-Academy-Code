8ff1ec07200850f2bc88ff8aba3afb0cbf15efab
ZachFaria <38866135+ZachFaria@users.noreply.github.com>
2018-10-15 11:14:37 -0400

    Create Whats_For_Dinner.js

---
+++ Whats_For_Dinner.js
@@ -0,0 +1,28 @@
+background(186, 145, 20); // wooden table
+
+// plate
+ellipse(200, 200, 350, 350);
+ellipse(200, 200, 300, 300); 
+
+// pizza slice 
+
+// base of pizza
+fill(255, 213, 0);
+triangle(374, 134, 116, 307, 64, 172);
+
+rotate(-21); // rotates drawn shapes
+
+fill(214, 102, 4);
+rect(-12, 183, 20, 146);
+
+// toppings
+fill(199, 2, 2);
+ellipse(53, 222, 40, 40);
+ellipse(125, 270, 40, 40);
+ellipse(170, 247, 40, 40);
+ellipse(81, 282, 40, 40);
+ellipse(36, 267, 40, 40);
+arc(197, 282, 40, 40, 167, 347);
+arc(39, 318, 40, 40, 167, 347);
+arc(256, 248, 40, 40, 14, 192);
+arc(107, 211, 40, 40, 14, 192);
