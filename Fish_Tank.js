6934e25eb7f028a45104ae473c9d58a0ad3f96fb
ZachFaria <38866135+ZachFaria@users.noreply.github.com>
2018-10-16 10:17:12 -0400

    Create Fish_Tank.js

---
+++ Fish_Tank.js
@@ -0,0 +1,38 @@
+// function to draw fish
+var drawFish = function(x, y, l, h, c, tw, e, tc) {
+    noStroke();
+    fill(c);
+    // body
+    ellipse(x, y, l, h);
+    // tail
+    fill(tc);
+    var tailWidth = tw/4;
+    var tailHeight = h/2;
+    triangle(x-l/2, y, x-l/2-tailWidth, y-h*1/1.5, x-l/2-tailWidth, y+h*1/1.5);
+    // eye
+    fill(33, 33, 33);
+    ellipse(x+l/4, y, e/5, e/5);
+};
+
+// background
+background(89, 216, 255);
+
+// function to draw bubbles
+var drawBubble = function() {
+    noStroke();
+    fill(0, 102, 255);
+    ellipse(random(10, 360), random(10, 360), 10, 10);
+};
+
+// bubbles
+drawBubble();
+drawBubble();
+drawBubble();
+drawBubble();
+drawBubble();
+drawBubble();
+
+// function to add a fish to the screen when the mouse is clicked
+mouseClicked = function() {
+    drawFish(mouseX, mouseY, random(10, 200), random(10, 200), color(random(0, 255), random(0, 255), random(0, 255)), random(50, 200), random(10, 200), color(random(0, 255), random(0, 255), random(0, 255)));
+};
